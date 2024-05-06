ff00b92d1a8ae1b388643524a9faa206ddde8a4b
Armin Jazi <a.ghajarjazi@reply.de>
2024-05-06 09:56:09 +0200

    fix config file

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { fileURLToPath, URL } from "url";
 import { create } from "@openreplyde/web-docker-vite-plugin";
 
-export default defineConfig(() => {
-  const base = import.meta.env.VITE_APP_BASE_PATH || "/";
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const base = env.VITE_APP_BASE_PATH || "/";
   return {
     plugins: [
       create({
